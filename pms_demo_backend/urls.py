950e59b6c74d559c94c02f9989e4fcd236a7fd8d
fish-chuan <newdavidlin@gmail.com>
2025-05-12 15:26:50 +0800

    disable admin/

--- pms_demo_backend/urls.py
+++ pms_demo_backend/urls.py
@@ -30,7 +30,7 @@ router.register('plan', views.PlanViewSet)
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='login'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
